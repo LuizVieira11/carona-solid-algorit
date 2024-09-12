044d7a5f3e63d081fb2b90730b6d436d5ce51c57
Luiz Gustav <luiz.vieira11@outlook.com>
2024-09-12 20:48:43 -0300

    Fiz o algoritmo distribuir passageiros entre todos os motoristas igualmente

--- python/test4.py
+++ python/test4.py
@@ -30,6 +30,7 @@ while True:
     if len(linha) == 0:
         break
     args = linha.split(",")
+    print(args)
     
     if eval(args[5]):
         motoristas.append(User(name=args[0], destination=args[1], maxPassengers=int(args[2]), 
@@ -42,16 +43,31 @@ G.add_nodes_from(passageiros, bipartite=0)  # Adicionando objetos inteiros
 G.add_nodes_from(motoristas, bipartite=1)  # Adicionando objetos inteiros
 
 arestas = []
+passageiros_alistados=[]
 
-
-
-# Criando as arestas com base no destino e disponibilidade de vagas
+# Distribuindo passageiros de maneira justa
 for p in passageiros:
-    for m in motoristas:
-        if p.destination == m.destination and m.reservedSeats < m.maxPassengers:
-            m.reservedSeats += 1
-            arestas.append((m, p))  # Armazenando objetos completos
-            break
+    motoristas_disponiveis = [m for m in motoristas if m.destination == p.destination and m.reservedSeats < m.maxPassengers]
+    if motoristas_disponiveis:
+        # Ordena os motoristas pela quantidade de vagas disponíveis, para priorizar motoristas menos ocupados
+        motoristas_disponiveis.sort(key=lambda m: m.reservedSeats)
+        motorista_selecionado = motoristas_disponiveis[0]
+        motorista_selecionado.reservedSeats += 1
+        passageiros_alistados.append(p)
+        arestas.append((motorista_selecionado, p))  # Conecta motorista e passageiro no grafo
+
+# limitation = 0
+# while limitation < 5:
+#     # Criando as arestas com base no destino e disponibilidade de vagas
+#     for p in passageiros:
+#         for m in motoristas:
+#             if p.destination == m.destination and m.reservedSeats < m.maxPassengers:
+#                 if m.reservedSeats <= limitation and p not in passageiros_alistados:
+#                     m.reservedSeats += 1
+#                     passageiros_alistados.append(p)
+#                     arestas.append((m, p))  # Armazenando objetos completos
+    # limitation += 1
+            
 
 G.add_edges_from(arestas)
 
@@ -68,7 +84,6 @@ associacao_motoristas = {}
 for motorista, passageiro in arestas:
     if motorista not in associacao_motoristas:
         associacao_motoristas[motorista] = []
-    
     
     associacao_motoristas[motorista].append(passageiro)
         
@@ -95,5 +110,5 @@ passageiros_nomes = [p.name for p in passageiros_sem_motorista]
 motoristas_nomes = [m.name for m in motoristas_sem_passageiro]
 print("\nPassageiros sem motorista:")
 print(", ".join(passageiros_nomes) if passageiros_sem_motorista else "Todos os passageiros foram emparelhados com motoristas.")
-print("\Motoristas sem passageiro:")
+print("\nMotoristas sem passageiro:")
 print(", ".join(motoristas_nomes) if motoristas_sem_passageiro else "Todos os motoristas foram emparelhados com passageiros.")
